b9a6fe84248945766c2b5790ff05cb6878d10b57
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-01-27 20:55:23 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Matheus F S L Gomes on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
