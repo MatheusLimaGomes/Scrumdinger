216edc8fb3d54bf4cd734e4026f5505d70e80412
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-01-28 11:53:14 -0300

    feat: Display a list of daily scrums (1 of3)

---
+++ Scrumdinger/Screens/ScrumView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumView.swift
+//  Scrumdinger
+//
+//  Created by Matheus F S L Gomes on 28/01/23.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumView(scrums: DailyScrum.sampleData)
+    }
+}
