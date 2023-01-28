1becdc8fa440948aef5fd0b0c82760338da99e8a
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-01-28 11:35:25 -0300

    chore: Customize label style

---
+++ Scrumdinger/Screens/LableStyle.swift
@@ -0,0 +1,20 @@
+//
+//  LableStyle.swift
+//  Scrumdinger
+//
+//  Created by Matheus F S L Gomes on 28/01/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
