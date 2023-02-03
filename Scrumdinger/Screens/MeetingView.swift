73de9b17f74c4baf18c36abccf5f9a7e59cf899d
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-02-03 20:42:12 -0300

    feat: Create an overlay view

--- Scrumdinger/Screens/MeetingView.swift
+++ Scrumdinger/Screens/MeetingView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 20)
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                ProgressView(value: 5, total: 20)
                 HStack {
                     VStack(alignment: .leading) {
                         Text("Seconds Elapsed")
@@ -26,22 +30,25 @@ struct MeetingView: View {
                         .accessibilityLabel("Time Remaining")
                         .accessibilityValue("10 minutes")
                 }
-            Circle().strokeBorder(lineWidth: 25)
-            HStack {
-                Text("Speaker 1 of 3 ")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
+                Circle().strokeBorder(lineWidth: 25)
+                HStack {
+                    Text("Speaker 1 of 3 ")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next Speaker")
                 }
-                .accessibilityLabel("Next Speaker")
             }
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.sampleData[1]))
     }
 }
