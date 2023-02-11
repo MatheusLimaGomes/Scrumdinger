9a24508873c5ebf52c1edd34a899bb8f92c30680
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-02-11 16:05:04 -0300

    feat: Draw the progress ring

--- Scrumdinger/Screens/Meeting/MeetingTimerView.swift
+++ Scrumdinger/Screens/Meeting/MeetingTimerView.swift
@@ -26,13 +26,26 @@ struct MeetingTimerView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
+            .overlay {
+                ForEach(speakers) { speaker in
+                    if speaker.isCompleted, let index = speakers.firstIndex(where: { $0.id == speaker.id }) {
+                        SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(theme.mainColor, lineWidth: 12)
+                    }
+                }
+            }
+            .padding(.horizontal)
     }
 }
 struct MeetingTimerView_Previews: PreviewProvider {
     static var speakers: [ScrumTimer.Speaker] {
-        [ScrumTimer.Speaker(name: "John", isCompleted: false)]
+        [ScrumTimer.Speaker(name: "John", isCompleted: true), ScrumTimer.Speaker(name: "Craig", isCompleted: true),
+         ScrumTimer.Speaker(name: "Bryan", isCompleted: false),
+         ScrumTimer.Speaker(name: "Lisa", isCompleted: false),
+         ScrumTimer.Speaker(name: "Charlotte", isCompleted: false)]
     }
     static var previews: some View {
-        MeetingTimerView(theme: .lavander, speakers: speakers)
+        MeetingTimerView(theme: .yellow, speakers: speakers)
     }
 }
