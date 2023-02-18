8a32b0f940dd1e0b5574cb578d882710a8aae698
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-02-18 11:01:49 -0300

    feat: integrate speech recognition

--- Scrumdinger/Screens/Meeting/MeetingView.swift
+++ Scrumdinger/Screens/Meeting/MeetingView.swift
@@ -12,7 +12,8 @@ struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
-    
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
@@ -36,10 +37,15 @@ struct MeetingView: View {
                 player.seek(to: .zero)
                 player.play()
             }
+            speechRecognizer.reset()
+            speechRecognizer.transcribe()
+            isRecording = true
             scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
+            speechRecognizer.stopTranscribing()
+            isRecording = false
             let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
             scrum.history.insert(newHistory, at: 0)
         }
