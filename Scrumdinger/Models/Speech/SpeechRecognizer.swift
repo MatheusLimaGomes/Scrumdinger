7e108fd42fb102796d87b0bffc030fb9339a1479
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2023-02-18 11:30:23 -0300

    fix: fix Speech Recognizer

--- Scrumdinger/Models/Speech/SpeechRecognizer.swift
+++ Scrumdinger/Models/Speech/SpeechRecognizer.swift
@@ -4,11 +4,10 @@
 //
 //  Created by Matheus F S L Gomes on 18/02/23.
 //
-
-import SwiftUI
 import AVFoundation
 import Foundation
 import Speech
+import SwiftUI
 
 class SpeechRecognizer: ObservableObject {
     enum RecognizerError: Error {
@@ -22,10 +21,11 @@ class SpeechRecognizer: ObservableObject {
             case .nilRecognizer: return "Can't initialize speech recognizer"
             case .notAuthorizedToRecognize: return "Not authorized to recognize speech"
             case .notPermittedToRecord: return "Not permitted to record audio"
-            case .recognizerIsUnavailable: return "Recognizer is unavaiable"
+            case .recognizerIsUnavailable: return "Recognizer is unavailable"
             }
         }
     }
+    
     var transcript: String = ""
     
     private var audioEngine: AVAudioEngine?
@@ -59,7 +59,7 @@ class SpeechRecognizer: ObservableObject {
     
     func transcribe() {
         DispatchQueue(label: "Speech Recognizer Queue", qos: .background).async { [weak self] in
-            guard let  self = self, let recognizer = recognizer?.isAvailable else {
+            guard let self = self, let recognizer = self.recognizer, recognizer.isAvailable else {
                 self?.speakError(RecognizerError.recognizerIsUnavailable)
                 return
             }
@@ -75,9 +75,11 @@ class SpeechRecognizer: ObservableObject {
             }
         }
     }
+    
     func stopTranscribing() {
         reset()
     }
+    
     func reset() {
         task?.cancel()
         audioEngine?.stop()
@@ -85,6 +87,7 @@ class SpeechRecognizer: ObservableObject {
         request = nil
         task = nil
     }
+    
     private static func prepareEngine() throws -> (AVAudioEngine, SFSpeechAudioBufferRecognitionRequest) {
         let audioEngine = AVAudioEngine()
         
@@ -94,10 +97,10 @@ class SpeechRecognizer: ObservableObject {
         let audioSession = AVAudioSession.sharedInstance()
         try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
         try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-        let inputMode = audioEngine.inputNode
+        let inputNode = audioEngine.inputNode
         
-        let recordingFormat = inputMode.outputFormat(forBus: 0)
-        inputMode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
             request.append(buffer)
         }
         audioEngine.prepare()
@@ -105,6 +108,7 @@ class SpeechRecognizer: ObservableObject {
         
         return (audioEngine, request)
     }
+    
     private func recognitionHandler(result: SFSpeechRecognitionResult?, error: Error?) {
         let receivedFinalResult = result?.isFinal ?? false
         let receivedError = error != nil
@@ -113,23 +117,27 @@ class SpeechRecognizer: ObservableObject {
             audioEngine?.stop()
             audioEngine?.inputNode.removeTap(onBus: 0)
         }
+        
         if let result = result {
             speak(result.bestTranscription.formattedString)
         }
     }
+    
     private func speak(_ message: String) {
         transcript = message
     }
+    
     private func speakError(_ error: Error) {
         var errorMessage = ""
         if let error = error as? RecognizerError {
-            errorMessage = error.message
+            errorMessage += error.message
         } else {
-            errorMessage = error.localizedDescription
+            errorMessage += error.localizedDescription
         }
         transcript = "<< \(errorMessage) >>"
     }
 }
+
 extension SFSpeechRecognizer {
     static func hasAuthorizationToRecognize() async -> Bool {
         await withCheckedContinuation { continuation in
@@ -139,6 +147,7 @@ extension SFSpeechRecognizer {
         }
     }
 }
+
 extension AVAudioSession {
     func hasPermissionToRecord() async -> Bool {
         await withCheckedContinuation { continuation in
